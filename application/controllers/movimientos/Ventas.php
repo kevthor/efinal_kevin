5a543f67f6f62082e3a8d1a6f0ff20187b4477e0
kevin1308 <42851518+kevin1308@users.noreply.github.com>
2019-10-03 21:43:21 -0400

    efinal_kevin
    este es el examen final de kevin torres

---
+++ application/controllers/movimientos/Ventas.php
@@ -0,0 +1,125 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ventas extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		if (!$this->session->userdata("login")) {
+			redirect(base_url());
+		}
+		$this->load->model("Ventas_model");
+		$this->load->model("Clientes_model");
+		$this->load->model("Productos_model");
+	}
+
+	public function index(){
+		$data  = array(
+			'ventas' => $this->Ventas_model->getVentas(), 
+		);
+		$this->load->view("layouts/header");
+		$this->load->view("layouts/aside");
+		$this->load->view("admin/ventas/list",$data);
+		$this->load->view("layouts/footer");
+	}
+
+	public function add(){
+		$data = array(
+			"tipocomprobantes" => $this->Ventas_model->getComprobantes(),
+			"clientes" => $this->Clientes_model->getClientes()
+		);
+		$this->load->view("layouts/header");
+		$this->load->view("layouts/aside");
+		$this->load->view("admin/ventas/add",$data);
+		$this->load->view("layouts/footer");
+	}
+
+	public function getproductos(){
+		$valor = $this->input->post("valor");
+		$clientes = $this->Ventas_model->getproductos($valor);
+		echo json_encode($clientes);
+	}
+
+	public function store(){
+		$fecha = $this->input->post("fecha");
+		$subtotal = $this->input->post("subtotal");
+		$igv = $this->input->post("igv");
+		$descuento = $this->input->post("descuento");
+		$total = $this->input->post("total");
+		$idcomprobante = $this->input->post("idcomprobante");
+		$id_cliente = $this->input->post("idcliente");
+		$id_usuario = $this->session->userdata("id");
+		$numero = $this->input->post("numero");
+		$serie_comprobante = $this->input->post("serie");
+
+		$idproductos = $this->input->post("idproductos");
+		$precios = $this->input->post("precios");
+		$cantidades = $this->input->post("cantidades");
+		$importes = $this->input->post("importes");
+
+		$data = array(
+			'fecha' => $fecha,
+			'subtotal' => $subtotal,
+			'igv' => $igv,
+			'descuento' => $descuento,
+			'total' => $total,
+			'tipo_comprobante' => $idcomprobante,
+			'id_cliente' => $idcliente,
+			'id_usuario' => $idusuario,
+			'nro_comprobante' => $numero,
+			'serie_comprobante' => $serie,
+		);
+
+		if ($this->Ventas_model->save($data)) {
+			$idventa = $this->Ventas_model->lastID();
+			$this->updateComprobante($idcomprobante);
+			$this->save_detalle($idproductos,$idventa,$precios,$cantidades,$importes);
+			redirect(base_url()."movimientos/ventas");
+
+		}else{
+			redirect(base_url()."movimientos/ventas/add");
+		}
+	}
+
+	protected function updateComprobante($idcomprobante){
+		$comprobanteActual = $this->Ventas_model->getComprobante($idcomprobante);
+		$data  = array(
+			'cantidad' => $comprobanteActual->cantidad + 1, 
+		);
+		$this->Ventas_model->updateComprobante($idcomprobante,$data);
+	}
+
+	protected function save_detalle($productos,$idventa,$precios,$cantidades,$importes){
+		for ($i=0; $i < count($productos); $i++) { 
+			$data  = array(
+				'id_producto' => $productos[$i], 
+				'id_venta' => $idventa,
+				'precio_unitario' => $precios[$i],
+				'cantidad' => $cantidades[$i],
+				'importe'=> $importes[$i],
+			);
+
+			$this->Ventas_model->save_detalle($data);
+			$this->updateProducto($productos[$i],$cantidades[$i]);
+
+		}
+	}
+
+	protected function updateProducto($idproducto,$cantidad){
+		$productoActual = $this->Productos_model->getProducto($idproducto);
+		$data = array(
+			'stock' => $productoActual->stock - $cantidad, 
+		);
+		$this->Productos_model->update($idproducto,$data);
+	}
+
+	public function view(){
+		$idventa = $this->input->post("id");
+		$data = array(
+			"venta" => $this->Ventas_model->getVenta($idventa),
+			"detalles" =>$this->Ventas_model->getDetalle($idventa)
+		);
+		$this->load->view("admin/ventas/view",$data);
+	}
+
+}
